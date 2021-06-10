c9c1db9d07763034186c6fb1e733a942f561dc20
weitao <wangwt62@chinaunicom.cn>
2021-06-10 13:21:48 +0800

    Grid Component Test

---
+++ ui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GridTest from './components/GridTest';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GridTest />
+    </React.Fragment>
+  );
+}
+
+export default App;

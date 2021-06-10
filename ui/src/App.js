778e3a63c4051a0a6e3061baf9239dacd450af4a
weitao <wangwt62@chinaunicom.cn>
2021-06-10 18:35:08 +0800

    Button Link Test

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GridTest from './components/GridTest';
+import LinkTest from './components/LinkTest';
 
 const App = () => {
   return (
     <React.Fragment>
       <GridTest />
+      <LinkTest />
     </React.Fragment>
   );
 }
